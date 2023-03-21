3d8a0d017e515fd59eba620b638c49a6a1b91466
miklos.petronia@hotmail.com <miklos.petronia@hotmail.com>
2023-03-21 06:11:44 +0100

    import apollo dependency

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -2,3 +2,6 @@
 import React, { useEffect, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
+
+// import apollo dependency
+import { useQuery } from '@apollo/client';
