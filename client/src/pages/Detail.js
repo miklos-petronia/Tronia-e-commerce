3b82494fd779db634b63b997dbb6b5153ddacb32
miklos.petronia@hotmail.com <miklos.petronia@hotmail.com>
2023-03-21 06:11:12 +0100

    import react dependencies

---
+++ client/src/pages/Detail.js
@@ -0,0 +1,4 @@
+// import react dependencies
+import React, { useEffect, useState } from 'react';
+import { Link, useParams } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
