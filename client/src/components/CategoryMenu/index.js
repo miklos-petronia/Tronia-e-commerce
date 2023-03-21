a34cafcb2467de675fd936fbecab8de053812bba
miklos.petronia@hotmail.com <miklos.petronia@hotmail.com>
2023-03-21 06:00:26 +0100

    update current category when button is clicked

--- client/src/components/CategoryMenu/index.js
+++ client/src/components/CategoryMenu/index.js
@@ -39,3 +39,30 @@ function CategoryMenu() {
             });
         }
     }, [categoryData, loading, dispatch]);
+
+    // update current category when button is clicked
+    const handleClick = (id) => {
+        dispatch({
+            type: UPDATE_CURRENT_CATEGORY,
+            currentCategory: id,
+        });
+    };
+
+    return (
+        <div>
+            <h2>Choose a Category:</h2>
+            {categories.map((item) => (
+                <button
+                    key={item._id}
+                    onClick={() => {
+                        handleClick(item._id);
+                    }}
+                >
+                    {item.name}
+                </button>
+            ))}
+        </div>
+    );
+}
+
+export default CategoryMenu;
