db1d8eb0b2f37a2067388186a235ff135e5070d1
miklos.petronia@hotmail.com <miklos.petronia@hotmail.com>
2023-03-21 05:43:25 +0100

    import stripe dependency

--- client/src/components/Cart/index.js
+++ client/src/components/Cart/index.js
@@ -4,3 +4,6 @@ import { useDispatch, useSelector } from 'react-redux';
 
 // import apollo dependency
 import { useLazyQuery } from '@apollo/client';
+
+// import stripe dependency
+import { loadStripe } from '@stripe/stripe-js';
