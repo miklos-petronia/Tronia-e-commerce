6cc5d1891ec55a796cd62c04790e2146fea4e4ca
miklos.petronia@hotmail.com <miklos.petronia@hotmail.com>
2023-03-21 05:42:39 +0100

    import apollo dependency

--- client/src/components/Cart/index.js
+++ client/src/components/Cart/index.js
@@ -1,3 +1,6 @@
 // import react dependencies
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+
+// import apollo dependency
+import { useLazyQuery } from '@apollo/client';
