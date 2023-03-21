355135958f377097ddbd6b5eb53a42e5b445bc65
miklos.petronia@hotmail.com <miklos.petronia@hotmail.com>
2023-03-21 06:16:04 +0100

    remove item with currentProduct._id from cart

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -80,3 +80,43 @@ function Detail() {
             idbPromise('cart', 'put', { ...currentProduct, purchaseQuantity: 1 });
         }
     };
+
+    // remove item with currentProduct._id from cart
+    const removeFromCart = () => {
+        dispatch({
+            type: REMOVE_FROM_CART,
+            _id: currentProduct._id,
+        });
+        idbPromise('cart', 'delete', { ...currentProduct });
+    };
+
+    return (
+        <>
+            {currentProduct && cart ? (
+                <div className="container my-1">
+                    <Link to="/">← Back to Products</Link>
+                    <h2>{currentProduct.name}</h2>
+                    <p>{currentProduct.description}</p>
+                    <p>
+                        <strong>Price:</strong>${currentProduct.price}{' '}
+                        <button onClick={addToCart}>Add to Cart</button>
+                        <button
+                            disabled={!cart.find((p) => p._id === currentProduct._id)}
+                            onClick={removeFromCart}
+                        >
+                            Remove from Cart
+                        </button>
+                    </p>
+                    <img
+                        src={`/images/${currentProduct.image}`}
+                        alt={currentProduct.name}
+                    />
+                </div>
+            ) : null}
+            {loading ? <img src={spinner} alt="loading" /> : null}
+            <Cart />
+        </>
+    );
+}
+
+export default Detail;
