a920c70db118113cb153e203fdc11470db8dfbba
miklos.petronia@hotmail.com <miklos.petronia@hotmail.com>
2023-03-21 06:05:30 +0100

    import utils dependencies

--- client/src/components/ProductItem/index.js
+++ client/src/components/ProductItem/index.js
@@ -2,3 +2,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { useDispatch, useSelector } from 'react-redux';
+
+// import utils dependencies
+import { ADD_TO_CART, UPDATE_CART_QUANTITY } from "../../utils/actions";
+import { pluralize } from "../../utils/helpers"
+import { idbPromise } from "../../utils/helpers";
+
+function ProductItem(item) {
+    const dispatch = useDispatch();
+    const state = useSelector(state => state);
+    const { image, name, _id, price, quantity } = item;
+    const { cart } = state;
