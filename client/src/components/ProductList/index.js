b71097879ec1621c62461db9f59a1f65617851c0
miklos.petronia@hotmail.com <miklos.petronia@hotmail.com>
2023-03-21 06:10:17 +0100

    return products if currentCategory does not exist, then filter and return products that match product.category._id

--- client/src/components/ProductList/index.js
+++ client/src/components/ProductList/index.js
@@ -44,3 +44,40 @@ function ProductList() {
             });
         }
     }, [data, loading, dispatch]);
+
+    // return products if currentCategory does not exist, then filter and return products that match product.category._id
+    function filterProducts() {
+        if (!currentCategory) {
+            return state.products;
+        }
+
+        return state.products.filter(
+            (product) => product.category._id === currentCategory
+        );
+    }
+
+    return (
+        <div className="my-2">
+            <h2>Our Products:</h2>
+            {state.products.length ? (
+                <div className="flex-row">
+                    {filterProducts().map((product) => (
+                        <ProductItem
+                            key={product._id}
+                            _id={product._id}
+                            image={product.image}
+                            name={product.name}
+                            price={product.price}
+                            quantity={product.quantity}
+                        />
+                    ))}
+                </div>
+            ) : (
+                <h3>You haven't added any products yet!</h3>
+            )}
+            {loading ? <img src={spinner} alt="loading" /> : null}
+        </div>
+    );
+}
+
+export default ProductList;
