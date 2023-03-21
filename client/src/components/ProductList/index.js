9637b4bc1af9f213ef794ba1c7c554b912ffe891
miklos.petronia@hotmail.com <miklos.petronia@hotmail.com>
2023-03-21 06:08:07 +0100

    import utils dependencies

--- client/src/components/ProductList/index.js
+++ client/src/components/ProductList/index.js
@@ -4,3 +4,8 @@ import { useDispatch, useSelector } from 'react-redux';
 
 // import apollo dependency
 import { useQuery } from '@apollo/client';
+
+// import utils dependencies
+import { UPDATE_PRODUCTS } from '../../utils/actions';
+import { QUERY_PRODUCTS } from '../../utils/queries';
+import { idbPromise } from '../../utils/helpers';
