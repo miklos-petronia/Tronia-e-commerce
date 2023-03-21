955b294b496b4201c81646ae262eaf8506565ad5
miklos.petronia@hotmail.com <miklos.petronia@hotmail.com>
2023-03-21 06:44:06 +0100

    import the action file

---
+++ client/src/utils/reducers.js
@@ -0,0 +1,12 @@
+// import the action file
+import {
+    UPDATE_PRODUCTS,
+    ADD_TO_CART,
+    UPDATE_CART_QUANTITY,
+    REMOVE_FROM_CART,
+    ADD_MULTIPLE_TO_CART,
+    UPDATE_CATEGORIES,
+    UPDATE_CURRENT_CATEGORY,
+    CLEAR_CART,
+    TOGGLE_CART
+} from "./actions";
