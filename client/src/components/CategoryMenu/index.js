76a7fbaf00ec796271d123d68aa76d34c4936c5a
miklos.petronia@hotmail.com <miklos.petronia@hotmail.com>
2023-03-21 05:58:15 +0100

    import utils dependencies

--- client/src/components/CategoryMenu/index.js
+++ client/src/components/CategoryMenu/index.js
@@ -4,3 +4,14 @@ import { useDispatch, useSelector } from 'react-redux';
 
 // import apollo dependency
 import { useQuery } from '@apollo/client';
+
+// import utils dependencies
+import { UPDATE_CATEGORIES, UPDATE_CURRENT_CATEGORY } from '../../utils/actions';
+import { QUERY_CATEGORIES } from '../../utils/queries';
+import { idbPromise } from '../../utils/helpers';
+
+function CategoryMenu() {
+    const dispatch = useDispatch();
+    const state = useSelector(state => state);
+    const { categories } = state;
+    const { loading, data: categoryData } = useQuery(QUERY_CATEGORIES);
