d6edff1a87c3bc7f78184e171664e5382f92b849
miklos.petronia@hotmail.com <miklos.petronia@hotmail.com>
2023-03-21 06:48:11 +0100

    import apollo dependencies

--- client/src/App.js
+++ client/src/App.js
@@ -2,3 +2,12 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Provider } from 'react-redux';
+
+// import apollo dependencies
+import {
+    ApolloClient,
+    InMemoryCache,
+    ApolloProvider,
+    createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
