04e39b4d86b6b30f895018466e7772924df791dc
miklos.petronia@hotmail.com <miklos.petronia@hotmail.com>
2023-03-21 05:54:29 +0100

    import utils dependencies

--- client/src/components/CartItem/index.js
+++ client/src/components/CartItem/index.js
@@ -1,3 +1,10 @@
 // import react dependencies
 import React from 'react';
 import { useDispatch } from 'react-redux';
+
+// import utils dependencies
+import { REMOVE_FROM_CART, UPDATE_CART_QUANTITY } from "../../utils/actions";
+import { idbPromise } from "../../utils/helpers";
+
+const CartItem = ({ item }) => {
+    const dispatch = useDispatch();
